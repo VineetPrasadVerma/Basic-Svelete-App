141894f1637c0328a462c9d974aab852bf75d381
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-04-25 03:29:03 +0530

    Learn about loops

--- my-project/src/App.svelte
+++ my-project/src/App.svelte
@@ -1,33 +1,50 @@
 <script>
-	// let name = 'My First Svelte App';
-	let beltColor = 'Black'
-	let firstName = 'Vineet'
-	let lastName = 'Verma'
+	// // let name = 'My First Svelte App';
+	// let beltColor = 'Black'
+	// let firstName = 'Vineet'
+	// let lastName = 'Verma'
 
-	const handleClick = () =>{
-		beltColor = 'Orange'
+	// const handleClick = () =>{
+	// 	beltColor = 'Orange'
+	// }
+
+	// const handleInput = (e) => {
+	// 	beltColor = e.target.value
+	// }
+
+	// $:fullName =  `${firstName} ${lastName}` // reactivity
+
+	// $: {
+	// 	console.log(beltColor)
+	// 	console.log(firstName)
+	// }
+	let persons = [
+		{name:'Vineet', age:22, id:1},
+		{name:'Vikas', age:24, id:2},
+		{name:'Vinay', age:23, id:3}
+	]
+
+	const handleClick = (id) => {
+		persons = persons.filter(p => p.id !== id)
 	}
-
-	const handleInput = (e) => {
-		beltColor = e.target.value
-	}
-
-	$:fullName =  `${firstName} ${lastName}` // reactivity
-
-	$: {
-		console.log(beltColor)
-		console.log(firstName)
-	}
-
-	</script>
+</script>
 
 <main>
-	<p>{fullName} has {beltColor} Belt</p>
+	<!-- <p>{fullName} has {beltColor} Belt</p>
 	<!-- <buttton on:click={handleClick}>Change Belt Color</buttton> -->
 	<!-- <input type="text" on:input={handleInput} value={beltcolor}> -->
-	<input type="text" bind:value={beltColor}>
+	<!-- <input type="text" bind:value={beltColor}>
 	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
+	<input type="text" bind:value={lastName}> -->
+
+	{#each persons as person (person.id)}
+		<div>
+			<h4>{person.name} is {person.age} year's old</h4>
+			<button on:click={(e) => handleClick(person.id) }>Delete</button>
+		</div>	
+		{:else}
+			<p>Nothing to show</p>
+	{/each}
 </main>
 
 <style>
