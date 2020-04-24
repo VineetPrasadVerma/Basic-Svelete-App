4a375afcbc14e92ef74cca1d0c5a40c1d740ce76
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-04-25 01:37:26 +0530

    learnt on click in svelte

--- my-project/src/App.svelte
+++ my-project/src/App.svelte
@@ -1,10 +1,16 @@
 <script>
-	export let name;
+	let name = 'My First Svelte App';
+	let beltcolor = 'Black Belt'
+
+	const handleClick = () =>{
+		beltcolor = 'Orange Belt'
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{name}</h1>
+	<p>{beltcolor}</p>
+	<buttton on:click={handleClick}>Change Belt Color</buttton>
 </main>
 
 <style>
