9a2795b1fe230251edad7f8dca820212eb19cc86
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-04-27 15:20:00 +0530

    Polls bars left

--- PollsApp/src/App.svelte
+++ PollsApp/src/App.svelte
@@ -1,11 +1,70 @@
 <script>
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+	import CreatePollForm from './components/CreatePollForm.svelte'
+	import PollList from './components/PollList.svelte'
+	import Tabs from './shared/Tabs.svelte'
 
+	//tabs
+	let items = ['Current Polls', 'Add New Poll']
+	let activeItem = 'Current Polls'
+
+	let polls = [
+		{
+			id: 1,
+			question: 'Python or JavaScript?',
+			answerA: 'Python',
+			answerB: 'JavaScript',
+			votesA: 9,
+			votesB: 15,
+    	},
+	]
+
+	const tabChange = (e) => {
+		activeItem = e.detail
+	} 
+
+	const handleAdd = (e) => {
+		const poll = e.detail
+		polls = [poll, ...polls]
+		activeItem = 'Current Polls'
+		console.log(polls)
+  	}
+
+	const handleVote = (e) => {
+		const {id, option} = e.detail
+
+		let copiedPolls = [...polls]
+		let upvotePoll = copiedPolls.find((poll) => poll.id === id)
+
+		if(option === 'a'){
+			upvotePoll.votesA++
+		}
+
+		if(option === 'b'){
+			upvotePoll.votesB++
+		}
+
+		polls = copiedPolls
+	}
 </script>
 
+<Header/>
+
 <main>
-	<h1>Hello</h1>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<PollList {polls} on:vote={handleVote}/>
+	{:else if activeItem === 'Add New Poll'}
+		<CreatePollForm on:add={handleAdd}/>
+	{/if}
 </main>
 
+<Footer/>
+
 <style>
-	
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
---
+++ PollsApp/src/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+    <h1>
+        <img src="/img/pollsLogo.png" alt="Polls Logo">
+    </h1>
+</header>
+
+<style>
+    header{
+        background: #f7f7ff;
+        padding: 20px;
+    }
+
+    h1{
+        margin: 0;
+        text-align: center;
+    }
+
+    img{
+        max-width: 120px;
+    }
+</style>
